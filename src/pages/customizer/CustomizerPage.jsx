fc38cd6919baf768c9e2e33da23c896bbee81d69
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 10:39:34 -0900

    handle switching between different cases : logo decal and full model decal

--- src/pages/customizer/CustomizerPage.jsx
+++ src/pages/customizer/CustomizerPage.jsx
@@ -6,15 +6,16 @@ import ColorPicker from "../../components/ColorPicker";
 import CustomBtn from "../../components/CustomBtn";
 import FilePicker from "../../components/FilePicker";
 import Tab from "../../components/Tab";
-import { EditorTabs, FilterTabs } from "../../config/constants";
+import { DecalTypes, EditorTabs, FilterTabs } from "../../config/constants";
 import { fadeAnimation, slideAnimation } from "../../config/motion";
 import proxyState from "../../proxyStore/proxy";
+import { reader } from "../../config/helpers";
 
 function CustomizerPage() {
 	const snap = useSnapshot(proxyState);
 	const [file, setFile] = useState("");
 	const [activeEditorTab, setActiveEditorTab] = useState("");
-	const [activeFilterTab, setactiveFilterTab] = useState({
+	const [activeFilterTab, setActiveFilterTab] = useState({
 		logoShirt: true,
 		styleShirt: false,
 	});
@@ -25,10 +26,45 @@ function CustomizerPage() {
 			case "colorpicker":
 				return <ColorPicker />;
 			case "filepicker":
-				return <FilePicker />;
+				return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
 			default:
 				return null;
 		}
+	}
+
+	function handleActiveFilterTab(tabName) {
+		switch (tabName) {
+			case "logoShirt":
+				proxyState.isLogoTexture = !activeFilterTab[tabName];
+				break;
+			case "stylishShirt":
+				proxyState.isFullTexture = !activeEditorTab[tabName];
+				break;
+			default:
+				proxyState.isFullTexture = true;
+				proxyState.isLogoTexture = false;
+				break;
+		}
+
+		setActiveFilterTab((prevState) => {
+			return { ...prevState, [tabName]: !prevState[tabName] };
+		});
+	}
+
+	function handleDecals(type, result) {
+		const decalType = DecalTypes[type];
+		proxyState[decalType.stateProperty] = result;
+
+		if (!activeFilterTab[decalType.filterTab]) {
+			handleActiveFilterTab(decalType.filterTab);
+		}
+	}
+
+	function readFile(type) {
+		reader(file).then((result) => {
+			handleDecals(type, result);
+			setActiveEditorTab("");
+		});
 	}
 
 	return (
@@ -73,9 +109,9 @@ function CustomizerPage() {
 							<Tab
 								key={tab.name}
 								tab={tab}
-								handleClick={() => {}}
+								handleClick={() => handleActiveFilterTab(tab.name)}
 								isFilterTab
-								isActiveTab=""
+								isActiveTab={activeFilterTab[tab.name]}
 							/>
 						))}
 					</motion.div>
