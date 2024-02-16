6a0d1f63a7c0cc15b6739a7b4f233e4013881ce2
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 03:11:13 -0900

    add shadow to the canvas

--- src/canvas/Canvas.jsx
+++ src/canvas/Canvas.jsx
@@ -6,7 +6,12 @@ import ObjectModel from "./ObjectModel";
 
 export default function CanvasModel() {
 	return (
-		<Canvas>
+		<Canvas
+			shadows
+			camera={{ position: [0, 0, 0], fov: 50 }}
+			gl={{ preserveDrawingBuffer: true }}
+			className="w-full max-w-full h-full transition-all ease-in"
+		>
 			<ambientLight intensity={0.5} />
 			<Environment preset="city" />
 			<CameraReg>
