204340f11290f222e1156ce9325138ab0afced05
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 09:01:19 -0900

    handle color picker and file picker taps

--- src/pages/customizer/CustomizerPage.jsx
+++ src/pages/customizer/CustomizerPage.jsx
@@ -1,7 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useSnapshot } from "valtio";
 import CanvasModel from "../../canvas/Canvas";
+import ColorPicker from "../../components/ColorPicker";
 import CustomBtn from "../../components/CustomBtn";
+import FilePicker from "../../components/FilePicker";
 import Tab from "../../components/Tab";
 import { EditorTabs, FilterTabs } from "../../config/constants";
 import { fadeAnimation, slideAnimation } from "../../config/motion";
@@ -9,6 +12,25 @@ import proxyState from "../../proxyStore/proxy";
 
 function CustomizerPage() {
 	const snap = useSnapshot(proxyState);
+	const [file, setFile] = useState("");
+	const [activeEditorTab, setActiveEditorTab] = useState("");
+	const [activeFilterTab, setactiveFilterTab] = useState({
+		logoShirt: true,
+		styleShirt: false,
+	});
+
+	// show tab content depending on the acive tab
+	function generateTabContent() {
+		switch (activeEditorTab) {
+			case "colorpicker":
+				return <ColorPicker />;
+			case "filepicker":
+				return <FilePicker />;
+			default:
+				return null;
+		}
+	}
+
 	return (
 		<AnimatePresence>
 			{!snap.intro && (
@@ -20,8 +42,13 @@ function CustomizerPage() {
 						<div className="flex items-center min-h-screen">
 							<div className="editortabs-container tabs">
 								{EditorTabs.map((tab) => (
-									<Tab key={tab.name} tab={tab} handleClick={() => {}} />
+									<Tab
+										key={tab.name}
+										tab={tab}
+										handleClick={() => setActiveEditorTab(tab.name)}
+									/>
 								))}
+								{generateTabContent()}
 							</div>
 						</div>
 					</motion.div>
