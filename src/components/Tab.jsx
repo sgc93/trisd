b223528424eafc66bc4f915bd37e2f84029a1966
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 09:02:45 -0900

    handle tab clicking : handle switching between file picker and color picker

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,5 +1,29 @@
-function Tab() {
-	return <div>Tab</div>;
+import React from "react";
+import { useSnapshot } from "valtio";
+import proxyState from "../proxyStore/proxy";
+
+function Tab({ tab, isFilterTab, isActiveTab, handleClick }) {
+	const snap = useSnapshot(proxyState);
+	const activeStyles =
+		isFilterTab && isActiveTab
+			? { backgroundColor: snap.color, opacity: 0.5 }
+			: { backgroundColor: "transparent", opacity: 1 };
+	return (
+		<div
+			key={tab.name}
+			className={`tab-btn ${
+				isFilterTab ? "rounded-full glasmorphism" : "rounded-4"
+			}`}
+			onClick={handleClick}
+			style={activeStyles}
+		>
+			<img
+				src={tab.icon}
+				alt={tab.name}
+				className={`${isFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12"}`}
+			/>
+		</div>
+	);
 }
 
 export default Tab;
