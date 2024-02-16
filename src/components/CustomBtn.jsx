43cd25976cf1669beacefdfd810e529e8fd2c9b0
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 10:38:51 -0900

    update Custom Btn component's layout

--- src/components/CustomBtn.jsx
+++ src/components/CustomBtn.jsx
@@ -1,4 +1,5 @@
 import { useSnapshot } from "valtio";
+import { getContrastingColor } from "../config/helpers";
 import proxyState from "../proxyStore/proxy";
 
 function CustomBtn({ type, title, customStyles, handleClick }) {
@@ -7,7 +8,13 @@ function CustomBtn({ type, title, customStyles, handleClick }) {
 		if (type === "filled") {
 			return {
 				backgroundColor: snap.color,
-				color: "#fff",
+				color: getContrastingColor(snap.color),
+			};
+		} else if (type === "outline") {
+			return {
+				borderWidth: "1px",
+				borderColor: snap.color,
+				color: snap.color,
 			};
 		}
 	}
