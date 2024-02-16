7ee4d8e5208612d91a9d0feb2720eb71e1807281
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 02:37:51 -0900

    import backdrop shadow component

--- src/canvas/Canvas.jsx
+++ src/canvas/Canvas.jsx
@@ -1,5 +1,6 @@
 import { Center, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import BackdropShadow from "./BackdropShadow";
 import CameraReg from "./CameraReg";
 import ObjectModel from "./ObjectModel";
 
@@ -9,6 +10,7 @@ export default function CanvasModel() {
 			<ambientLight intensity={0.5} />
 			<Environment preset="city" />
 			<CameraReg>
+				<BackdropShadow />
 				<Center>
 					<ObjectModel />
 				</Center>
