aa6752fa0513f25314a908337dd1186c42285295
sgc93 <smachewgedefaw@gmail.com>
2024-02-16 09:33:12 -0900

    handle color customizing functionality

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,5 +1,18 @@
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+import proxyState from "../proxyStore/proxy";
+
 function ColorPicker() {
-	return <div>color picker</div>;
+	const snap = useSnapshot(proxyState);
+	return (
+		<div className="absolute left-full ml-3">
+			<SketchPicker
+				color={snap.color}
+				disableAlpha
+				onChange={(color) => (proxyState.color = color.hex)}
+			/>
+		</div>
+	);
 }
 
 export default ColorPicker;
