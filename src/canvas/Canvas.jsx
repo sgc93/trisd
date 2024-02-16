fb4d2afcc27917d3b009b0550bf2d93cb15ab4ba
sgc93 <smachewgedefaw@gmail.com>
2024-02-15 23:20:29 -0900

    create the canvas to handle the 3d thing

--- src/canvas/Canvas.jsx
+++ src/canvas/Canvas.jsx
@@ -1,5 +1,18 @@
-function Canvas() {
-	return <div>the canvas </div>;
-}
+import { Center, Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import CameraReg from "./CameraReg";
+import ObjectModel from "./ObjectModel";
 
-export default Canvas;
+export default function CanvasModel() {
+	return (
+		<Canvas>
+			<ambientLight intensity={0.5} />
+			<Environment preset="city" />
+			<CameraReg>
+				<Center>
+					<ObjectModel />
+				</Center>
+			</CameraReg>
+		</Canvas>
+	);
+}
