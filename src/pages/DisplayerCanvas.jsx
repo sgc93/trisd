226111265355c9d80968eaf044b5566722fe62cb
sgc93 <smachewgedefaw@gmail.com>
2024-02-21 12:46:45 -0900

    separate rendering from ui components

--- src/pages/DisplayerCanvas.jsx
+++ src/pages/DisplayerCanvas.jsx
@@ -1,9 +1,11 @@
-import { useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import { useSnapshot } from "valtio";
+import Loading from "../components/Loading";
 import proxyState from "../proxyStore/proxy";
 
-function DisplayCanvas({ glbData }) {
+function Model({ glbData }) {
 	const { scene, nodes, materials, error } = useGLTF(glbData);
 	const snap = useSnapshot(proxyState);
 
@@ -13,11 +15,25 @@ function DisplayCanvas({ glbData }) {
 	console.log(materials, "in canvas");
 	return (
 		snap.inCanvas && (
-			<div>
-				Model displayer page
+			<div className="display-canvas">
 				<Canvas>
+					<ambientLight />
+					<OrbitControls />
 					<primitive object={scene} />
 				</Canvas>
+			</div>
+		)
+	);
+}
+
+function DisplayCanvas({ glbData }) {
+	const snap = useSnapshot(proxyState);
+	return (
+		snap.inCanvas && (
+			<div className="display-canvas">
+				<Suspense fallback={<Loading message={"rendering ..."} />}>
+					<Model glbData={glbData} />
+				</Suspense>
 			</div>
 		)
 	);
