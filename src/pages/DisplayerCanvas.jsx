0e3bbdf760df53e1705ca12c393d7e7ba2b4f045
sgc93 <smachewgedefaw@gmail.com>
2024-02-21 12:06:01 -0900

    create canvas and displaye 3d models

--- src/pages/DisplayerCanvas.jsx
+++ src/pages/DisplayerCanvas.jsx
@@ -1,14 +1,26 @@
+import { useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { useSnapshot } from "valtio";
 import proxyState from "../proxyStore/proxy";
 
 function DisplayCanvas({ glbData }) {
-	// const { scene, error } = useGLTF(glbData);
+	const { scene, nodes, materials, error } = useGLTF(glbData);
 	const snap = useSnapshot(proxyState);
 
 	// if (error) console.log(error.message);
 	if (!glbData) return;
 
-	return snap.inCanvas && <div>Model displayer page</div>;
+	console.log(materials, "in canvas");
+	return (
+		snap.inCanvas && (
+			<div>
+				Model displayer page
+				<Canvas>
+					<primitive object={scene} />
+				</Canvas>
+			</div>
+		)
+	);
 }
 
 export default DisplayCanvas;
