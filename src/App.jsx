6e04b9303e997a2446585b52bb3aa740ab56d004
sgc93 <smachewgedefaw@gmail.com>
2024-02-21 00:10:50 -0900

    create class state for styling cutomized cursor dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Cursor from "./components/Cursor";
 import CustomizerPage from "./pages/CustomizerPage";
 import FeaturePage from "./pages/FeaturePage";
 import HomePage from "./pages/HomePage";
 
 function App() {
+	const [cursorClass, setCursorClass] = useState("def_cursor");
+	function changeCursorClass(newClass) {
+		setCursorClass(newClass);
+	}
 	return (
 		<main className="app transition-all ease-in">
-			<Cursor />
+			<Cursor cursorClass={cursorClass} />
 			<HomePage />
 			<CustomizerPage />
-			<FeaturePage />
+			<FeaturePage changeCursorClass={changeCursorClass} />
 		</main>
 	);
 }
