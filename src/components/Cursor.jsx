c7cac6f379e64e58647bcc2dd45e7c40232ce207
sgc93 <smachewgedefaw@gmail.com>
2024-02-20 23:18:22 -0900

    check if the customized cursor element is referenced correctly

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -8,8 +8,7 @@ function Cursor() {
 			const cursorPosX = e.clientX;
 			const cursorPosY = e.clientY;
 
-			if (cursor.current !== undefined) {
-				console.log(cursor, cursorPosX, cursorPosY);
+			if (cursor.current !== null) {
 				cursor.current.style.left = `${cursorPosX}px`;
 				cursor.current.style.top = `${cursorPosY}px`;
 			}
