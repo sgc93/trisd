026a97ebfd88c5a1f276f80a72e6680b1b165b25
sgc93 <smachewgedefaw@gmail.com>
2024-02-21 13:40:59 -0900

    create model controller pannel

--- src/pages/DisplayerCanvas.jsx
+++ src/pages/DisplayerCanvas.jsx
@@ -1,18 +1,20 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { motion } from "framer-motion";
+import { Suspense, useState } from "react";
+import { MdDragIndicator } from "react-icons/md";
 import { useSnapshot } from "valtio";
 import Loading from "../components/Loading";
+import Logo from "../components/Logo";
 import proxyState from "../proxyStore/proxy";
 
 function Model({ glbData }) {
-	const { scene, nodes, materials, error } = useGLTF(glbData);
 	const snap = useSnapshot(proxyState);
+	const { scene } = useGLTF(glbData);
 
 	// if (error) console.log(error.message);
 	if (!glbData) return;
 
-	console.log(materials, "in canvas");
 	return (
 		snap.inCanvas && (
 			<div className="display-canvas">
@@ -26,11 +28,34 @@ function Model({ glbData }) {
 	);
 }
 
+function ModelController() {
+	const [showDragIndicator, setShowDragIndicator] = useState(false);
+	return (
+		<motion.div
+			className="controller-box"
+			drag
+			dragElastic={1.18}
+			onHoverStart={() => setShowDragIndicator(true)}
+			onHoverEnd={() => setShowDragIndicator(false)}
+		>
+			<div className="model-controller">Controller</div>
+			<MdDragIndicator
+				className="drag_indicator"
+				color="white"
+				size={23}
+				fillOpacity={showDragIndicator ? 0.4 : 0}
+			/>
+		</motion.div>
+	);
+}
+
 function DisplayCanvas({ glbData }) {
 	const snap = useSnapshot(proxyState);
 	return (
 		snap.inCanvas && (
-			<div className="display-canvas">
+			<div className="display-canvas_page">
+				<Logo newClass={"display-logo"} />
+				<ModelController />
 				<Suspense fallback={<Loading message={"rendering ..."} />}>
 					<Model glbData={glbData} />
 				</Suspense>
