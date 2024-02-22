8d58a3cecab76a0afde762597a7aced05a7d991d
sgc93 <smachewgedefaw@gmail.com>
2024-02-22 08:11:35 -0900

    handle color change event in color picker and update proxy states based on the specified purpose of the color change

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -2,14 +2,23 @@ import { SketchPicker } from "react-color";
 import { useSnapshot } from "valtio";
 import proxyState from "../proxyStore/proxy";
 
-function ColorPicker() {
+function ColorPicker({ purpose }) {
 	const snap = useSnapshot(proxyState);
+	const isBg = purpose === "themeChange";
+	const isColor = purpose === "customize";
+	function performPurpose(color) {
+		if (isColor) {
+			proxyState.color = color.hex;
+		} else if (isBg) {
+			proxyState.bg = color.hex;
+		}
+	}
 	return (
-		<div className="absolute left-full ml-3">
+		<div className="colorPicker absolute left-full ml-3">
 			<SketchPicker
-				color={snap.color}
+				color={isColor ? snap.color : snap.bg}
 				disableAlpha
-				onChange={(color) => (proxyState.color = color.hex)}
+				onChange={(color) => performPurpose(color)}
 			/>
 		</div>
 	);
