2ec53e076815c4324404d36fb55216346c1739ab
sgc93 <smachewgedefaw@gmail.com>
2024-02-24 05:09:06 -0900

    make the code simple with no functionality change

--- src/components/CustomBtn.jsx
+++ src/components/CustomBtn.jsx
@@ -1,30 +1,6 @@
-import { useSnapshot } from "valtio";
-import { getContrastingColor } from "../config/helpers";
-import proxyState from "../proxyStore/proxy";
-
 function CustomBtn({ type, title, customStyles, handleClick }) {
-	const snap = useSnapshot(proxyState);
-	function generateStyle(type) {
-		if (type === "filled") {
-			return {
-				backgroundColor: snap.color,
-				color: getContrastingColor(snap.color),
-			};
-		} else if (type === "outline") {
-			return {
-				borderWidth: "1px",
-				borderColor: snap.color,
-				color: snap.color,
-			};
-		}
-	}
-
 	return (
-		<button
-			className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-			style={generateStyle(type)}
-			onClick={handleClick}
-		>
+		<button className={`custom-btn ${type}`} onClick={handleClick}>
 			{title}
 		</button>
 	);
