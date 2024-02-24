b43887790f9e4aac0556a7eca3a007a43a5140dd
sgc93 <smachewgedefaw@gmail.com>
2024-02-24 09:51:24 -0900

    handle setting new logo and resetting to the old logo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import EditPanel from "../components/EditPanel";
 import HomeContent from "../components/HomeContent";
 import Logo from "../components/Logo";
+import { reader } from "../config/helpers";
 import "./pages.css";
 
 function HomePage() {
+	const [newLogo, setNewLogo] = useState("");
+	const [logoUrl, setLogoUrl] = useState("");
+
+	function handleLogoUploading(e) {
+		const file = e.target.files[0];
+		try {
+			reader(file).then((url) => {
+				setNewLogo(file);
+				setLogoUrl(url);
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	function handleLogoReset() {
+		setNewLogo("");
+		setLogoUrl("");
+	}
 	return (
 		<section className="home">
-			<div className="home-model">
-				{/* <HomeCanvas /> */}
-			</div>
-			<Logo />
+			{/* <div className="home-model">
+				<HomeCanvas />
+			</div> */}
+			<Logo url={logoUrl} />
 			<HomeContent />
-			<EditPanel />
+			<EditPanel
+				newLogo={newLogo}
+				handleLogoUploading={handleLogoUploading}
+				handleLogoReset={handleLogoReset}
+			/>
 		</section>
 	);
 }
