6bbbeb87f690e795161570b75bd1c36253b64213
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 08:58:08 -0900

    add draggable indicator icon + handle on mouse enter and leave

--- src/components/MaterialEditor.jsx
+++ src/components/MaterialEditor.jsx
@@ -1,21 +1,39 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import DragIndicator from "../components/DragIndicator";
 import { textures } from "../config/constants";
 import "./components.css";
-
-function MaterialEditor({ changeMaterial }) {
+function MaterialEditor({ changeMaterial, hideHomeContents }) {
+	const [showDragIndicator, setShowDragIndicator] = useState(false);
 	return (
-		<div className="material-editor">
-			{textures.map((texture) => {
-				return (
-					<div
-						key={texture.id}
-						className="material-golden texture"
-						onClick={() => changeMaterial(texture.id)}
-					>
-						<img src={texture.avatar} alt={texture.id} />
-					</div>
-				);
-			})}
-		</div>
+		<motion.div
+			className="material-editor"
+			drag
+			dragElastic={1.18}
+			onMouseEnter={() => {
+				setShowDragIndicator(true);
+				hideHomeContents(true);
+			}}
+			onMouseLeave={() => {
+				setShowDragIndicator(false);
+				hideHomeContents(false);
+			}}
+		>
+			<div>
+				{textures.map((texture) => {
+					return (
+						<div
+							key={texture.id}
+							className="material-golden texture"
+							onClick={() => changeMaterial(texture.id)}
+						>
+							<img src={texture.avatar} alt={texture.id} />
+						</div>
+					);
+				})}
+			</div>
+			<DragIndicator showDragIndicator={showDragIndicator} />
+		</motion.div>
 	);
 }
 
