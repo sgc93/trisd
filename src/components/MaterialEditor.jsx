d2222d2441950ec6f6f7e9da8d420e264c7c253b
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 01:44:46 -0900

    create material editor panel

---
+++ src/components/MaterialEditor.jsx
@@ -0,0 +1,16 @@
+import "./components.css";
+
+function MaterialEditor() {
+	// const golden = useTexture(goldenTexture);
+	// const metalic = useTexture(metalicTexture);
+
+	return (
+		<div className="material-editor">
+			<div className="material-golden texture">G</div>
+			<div className="material-golden texture">M</div>
+			<div className="material-golden texture">C</div>
+		</div>
+	);
+}
+
+export default MaterialEditor;
