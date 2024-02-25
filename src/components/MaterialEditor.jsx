384bbd3e90c4366e0c44030ae42c443c5fe9f66e
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 10:35:20 -0900

    use proxy states to handle closing and opening edit panels

--- src/components/MaterialEditor.jsx
+++ src/components/MaterialEditor.jsx
@@ -1,48 +1,53 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { useSnapshot } from "valtio";
 import DragIndicator from "../components/DragIndicator";
 import { textures } from "../config/constants";
+import { homeProxy } from "../proxyStore/proxy";
 import "./components.css";
 function MaterialEditor({ changeMaterial }) {
 	const [showDragIndicator, setShowDragIndicator] = useState(false);
 	const [textureName, setTextureName] = useState("");
+	const snap = useSnapshot(homeProxy);
 
 	return (
-		<motion.div
-			className="material-editor"
-			drag
-			dragElastic={1.18}
-			onMouseEnter={() => {
-				setShowDragIndicator(true);
-			}}
-			onMouseLeave={() => {
-				setShowDragIndicator(false);
-			}}
-		>
-			<span
-				className={`panel-name glassmorphism ${
-					showDragIndicator ? "show" : "hide"
-				}`}
+		snap.showMaterials && (
+			<motion.div
+				className="material-editor"
+				drag
+				dragElastic={1.18}
+				onMouseEnter={() => {
+					setShowDragIndicator(true);
+				}}
+				onMouseLeave={() => {
+					setShowDragIndicator(false);
+				}}
 			>
-				{textureName ? textureName : ""} Texture
-			</span>
-			<div>
-				{textures.map((texture) => {
-					return (
-						<div
-							key={texture.id}
-							className="material-golden texture"
-							onClick={() => changeMaterial(texture.id)}
-							onMouseEnter={() => setTextureName(texture.id)}
-							onMouseLeave={() => setTextureName("")}
-						>
-							<img src={texture.avatar} alt={texture.id} />
-						</div>
-					);
-				})}
-			</div>
-			<DragIndicator showDragIndicator={showDragIndicator} />
-		</motion.div>
+				<span
+					className={`panel-name glassmorphism ${
+						showDragIndicator ? "show" : "hide"
+					}`}
+				>
+					{textureName ? textureName : ""} Texture
+				</span>
+				<div>
+					{textures.map((texture) => {
+						return (
+							<div
+								key={texture.id}
+								className="material-golden texture"
+								onClick={() => changeMaterial(texture.id)}
+								onMouseEnter={() => setTextureName(texture.id)}
+								onMouseLeave={() => setTextureName("")}
+							>
+								<img src={texture.avatar} alt={texture.id} />
+							</div>
+						);
+					})}
+				</div>
+				<DragIndicator showDragIndicator={showDragIndicator} />
+			</motion.div>
+		)
 	);
 }
 
