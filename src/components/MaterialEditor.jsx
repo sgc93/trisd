aaed30198e8d52298ee7de331638c7f140ae8cd2
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 02:13:46 -0900

    add texture example images

--- src/components/MaterialEditor.jsx
+++ src/components/MaterialEditor.jsx
@@ -1,14 +1,24 @@
+import clayImg from "../../public/textures/earth/clay.png";
+import goldenImg from "../../public/textures/golden/golden.svg";
+import metalImg from "../../public/textures/metalic/metal.svg";
+import terrazzoImg from "../../public/textures/terrazoSlab/terrazzo.png";
 import "./components.css";
 
-function MaterialEditor() {
-	// const golden = useTexture(goldenTexture);
-	// const metalic = useTexture(metalicTexture);
-
+function MaterialEditor({ changeMaterial }) {
 	return (
 		<div className="material-editor">
-			<div className="material-golden texture">G</div>
-			<div className="material-golden texture">M</div>
-			<div className="material-golden texture">C</div>
+			<div className="material-golden texture">
+				<img src={metalImg} alt="metalic" />
+			</div>
+			<div className="material-golden texture">
+				<img src={goldenImg} alt="golden" />
+			</div>
+			<div className="material-golden texture">
+				<img src={clayImg} alt="golden" />
+			</div>
+			<div className="material-golden texture">
+				<img src={terrazzoImg} alt="golden" />
+			</div>
 		</div>
 	);
 }
