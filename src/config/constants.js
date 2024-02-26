183b316cc82fb649f57151c99ff9ac25c5cab678
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 23:25:39 -0900

    add texture constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -73,6 +73,19 @@ export const textures = [
 		avatar: "./textures/metalic/metal.svg",
 		current: true,
 	},
+
+	{
+		id: "mosaic",
+		texture: {
+			map: "./textures/mosaic/TilesMosaicPennyround001_COL_2K.png",
+			normalMap: "./textures/mosaic/TilesMosaicPennyround001_NRM_2K.png",
+			roughnessMap: "./textures/mosaic/TilesMosaicPennyround001_BUMP_2K.png",
+			aoMap: "./textures/mosaic/TilesMosaicPennyround001_AO_2K.png",
+			dispMap: "./textures/mosaic/TilesMosaicPennyround001_DISP_2K.png",
+		},
+		avatar: "./textures/mosaic/mosaic.png",
+		current: false,
+	},
 	{
 		id: "terrazzo",
 		texture: {
@@ -83,6 +96,61 @@ export const textures = [
 			aoMap: "./textures/terrazzoSlab/TerrazzoSlab028_AO_2K_METALNESS.png",
 		},
 		avatar: "./textures/terrazzoSlab/terrazzo.png",
+		current: false,
+	},
+];
+
+export const floorTextures = [
+	{
+		id: "reflector",
+		texture: {},
+		avatar: "./textures/none.png",
+		current: true,
+	},
+	{
+		id: "bronze",
+		texture: {
+			map: "./textures/bronze/MetalBronzeWorn001_COL_2K_METALNESS.png",
+			normalMap: "./textures/bronze/MetalBronzeWorn001_NRM_2K_METALNESS.png",
+			roughnessMap:
+				"./textures/bronze/MetalBronzeWorn001_ROUGHNESS_2K_METALNESS.png",
+			dispMap: "./textures/bronze/MetalBronzeWorn001_DISP_2K_METALNESS.png",
+		},
+		avatar: "./textures/bronze/bronze.png",
+		current: false,
+	},
+	{
+		id: "metalic",
+		texture: {
+			map: "./textures/metalic/Metal_006_basecolor.jpg",
+			normalMap: "./textures/metalic/Metal_006_normal.jpg",
+			roughnessMap: "./textures/metalic/Metal_006_roughness.jpg",
+			aoMap: "./textures/metalic/Metal_006_ambientOcclusion.jpg",
+		},
+		avatar: "./textures/metalic/metal.svg",
+		current: false,
+	},
+	{
+		id: "mosaic",
+		texture: {
+			map: "./textures/mosaic/TilesMosaicPennyround001_COL_2K.png",
+			normalMap: "./textures/mosaic/TilesMosaicPennyround001_NRM_2K.png",
+			roughnessMap: "./textures/mosaic/TilesMosaicPennyround001_BUMP_2K.png",
+			aoMap: "./textures/mosaic/TilesMosaicPennyround001_AO_2K.png",
+			dispMap: "./textures/mosaic/TilesMosaicPennyround001_DISP_2K.png",
+		},
+		avatar: "./textures/mosaic/mosaic.png",
+		current: false,
+	},
+	{
+		id: "oak",
+		texture: {
+			map: "./textures/oak/oak_col.png",
+			normalMap: "./textures/oak/oak_nrm.png",
+			roughnessMap: "./textures/oak/oak_roughness.png",
+			dispMap: "./textures/oak/oak_disp.png",
+		},
+		avatar: "./textures/oak/oak.png",
 		current: false,
 	},
 ];
