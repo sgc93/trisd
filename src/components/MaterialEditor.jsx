d7cea7e7e4074b74159406d6a4661782c9a1c5c0
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 23:26:54 -0900

    handle material editor panel for floor editing

--- src/components/MaterialEditor.jsx
+++ src/components/MaterialEditor.jsx
@@ -2,32 +2,93 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import { useSnapshot } from "valtio";
 import DragIndicator from "../components/DragIndicator";
-import { textures } from "../config/constants";
+import { floorTextures, textures } from "../config/constants";
 import { homeProxy } from "../proxyStore/proxy";
 import "./components.css";
-function MaterialEditor({ changeMaterial }) {
+function MaterialEditor({ changeMaterial, changeTMaterial, style, type }) {
 	const [showDragIndicator, setShowDragIndicator] = useState(false);
 	const [textureName, setTextureName] = useState("");
 	const [textureList, setTextureList] = useState(textures);
+	const [floorTList, setFloorTList] = useState(floorTextures);
 	const snap = useSnapshot(homeProxy);
 
 	function circleCurrentMaterial(id) {
-		const newLlist = textureList.filter((texture) => {
-			if (texture.id === id) {
-				texture.current = true;
-				return texture;
-			} else {
-				texture.current = false;
-				return texture;
-			}
-		});
-		setTextureList((textureList) => newLlist);
+		if (type === "floor") {
+			const newLlist = floorTList.filter((texture) => {
+				if (texture.id === id) {
+					texture.current = true;
+					return texture;
+				} else {
+					texture.current = false;
+					return texture;
+				}
+			});
+			setFloorTList((textureList) => newLlist);
+		} else if (type === "object") {
+			const newLlist = textureList.filter((texture) => {
+				if (texture.id === id) {
+					texture.current = true;
+					return texture;
+				} else {
+					texture.current = false;
+					return texture;
+				}
+			});
+			setTextureList((textureList) => newLlist);
+		}
+	}
+	if (type === "floor") {
+		return (
+			<motion.div
+				className="material-editor"
+				style={type === "floor" ? { zIndex: 999, top: "3rem" } : {}}
+				drag
+				dragElastic={1.18}
+				onMouseEnter={() => {
+					setShowDragIndicator(true);
+				}}
+				onMouseLeave={() => {
+					setShowDragIndicator(false);
+				}}
+			>
+				<span
+					className={`panel-name glassmorphism ${
+						showDragIndicator ? "show" : "hide"
+					}`}
+				>
+					{textureName ? textureName : ""} Texture
+				</span>
+				<div>
+					{floorTList &&
+						floorTList.map((texture) => {
+							return (
+								<div
+									key={texture.id}
+									className={`texture ${
+										texture.current ? "texture-current_floor" : ""
+									}`}
+									onClick={() => {
+										changeTMaterial(texture.id);
+										circleCurrentMaterial(texture.id);
+									}}
+									onMouseEnter={() => setTextureName(texture.id)}
+									onMouseLeave={() => setTextureName("")}
+								>
+									<img src={texture.avatar} alt={texture.id} />
+								</div>
+							);
+						})}
+				</div>
+				<DragIndicator showDragIndicator={showDragIndicator} />
+			</motion.div>
+		);
 	}
 
 	return (
 		snap.showMaterials && (
 			<motion.div
 				className="material-editor"
+				style={{ bottom: "4rem" }}
 				drag
 				dragElastic={1.18}
 				onMouseEnter={() => {
