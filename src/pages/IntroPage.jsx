745e572727255b12a1c449d922ef628820162271
sgc93 <smachewgedefaw@gmail.com>
2024-02-26 08:26:58 -0900

    create and some content into intro page

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,5 +1,18 @@
+import { useSnapshot } from "valtio";
+import Loading from "../components/Loading";
+import proxyState from "../proxyStore/proxy";
+import "./pages.css";
+
 function IntroPage() {
-	return <div>TRISD</div>;
+	const snap = useSnapshot(proxyState);
+	return (
+		snap.inIntro &&
+		snap.inHome && (
+			<section className="intro">
+				<Loading message={"Welcome to TRISD 👋"} />
+			</section>
+		)
+	);
 }
 
 export default IntroPage;
