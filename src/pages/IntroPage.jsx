9168ceea4607e4b5678a43065a8d233b2dcfd489
sgc93 <smachewgedefaw@gmail.com>
2024-02-26 11:38:38 -0900

    add conditionally displayable button to redirect ot the homepage

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,9 +1,9 @@
+import { motion } from "framer-motion";
 import { useSnapshot } from "valtio";
 import Loading from "../components/Loading";
 import LogoSVG from "../components/LogoSVG";
 import proxyState, { introProxy } from "../proxyStore/proxy";
 import "./pages.css";
-
 function IntroPage() {
 	const snap = useSnapshot(proxyState);
 	const bntSnap = useSnapshot(introProxy);
@@ -11,7 +11,7 @@ function IntroPage() {
 		snap.inIntro &&
 		snap.inHome && (
 			<section className="intro">
-				<LogoSVG />
+				{bntSnap.showBtn && <LogoSVG />}
 				<div className="intro-loading">
 					{bntSnap.showBtn ? (
 						<span>TRISD is ready to serve you!</span>
@@ -19,6 +19,11 @@ function IntroPage() {
 						<Loading message={"rendering..."} type={"notify-intro"} />
 					)}
 				</div>
+				{bntSnap.showBtn && (
+					<motion.button drag className="intro-btn glassmorphism">
+						go to TRISD
+					</motion.button>
+				)}
 			</section>
 		)
 	);
