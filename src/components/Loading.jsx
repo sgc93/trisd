21006f9aa7c6fb5f76da00d6f517dcbc570173dc
sgc93 <smachewgedefaw@gmail.com>
2024-02-26 08:28:56 -0900

    add spinner for loading purpose

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,15 @@
 import "./components.css";
 
-function Loading({ message }) {
+function Loading({ message, type }) {
+	if (type === "notify") {
+		console.log("loading type: " + type);
+		return (
+			<div className="notify-spinner">
+				<div className="only-spinner"></div>
+				<span>loading...</span>
+			</div>
+		);
+	}
 	return (
 		<div className="render-loading_spinner glassmorphism">
 			<div className="message">{message}</div>
