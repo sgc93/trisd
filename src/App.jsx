860dc0ef09f04d19a3226b7dd1a67da219b35aef
sgc93 <smachewgedefaw@gmail.com>
2024-02-26 08:25:20 -0900

    add intro page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import { useSnapshot } from "valtio";
 import Cursor from "./components/Cursor";
 import DisplayPage from "./pages/DisplayPage";
 import HomePage from "./pages/HomePage";
+import IntroPage from "./pages/IntroPage";
 import UploadPage from "./pages/UploadPage";
+import proxyState from "./proxyStore/proxy";
 
 function App() {
+	const snap = useSnapshot(proxyState);
 	const [glbData, setGlbData] = useState("./jebena.glb");
-
 	const [cursorClass, setCursorClass] = useState("def_cursor");
+
 	return (
 		<main className="app transition-all ease-in relative">
 			<Cursor cursorClass={cursorClass} />
+			<IntroPage />
 			<HomePage />
 			<UploadPage setGlbData={setGlbData} />
 			<DisplayPage glbData={glbData} />
