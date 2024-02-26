2073cfe368ce7fda9287b96dfdcb21be9b40a650
sgc93 <smachewgedefaw@gmail.com>
2024-02-26 02:08:47 -0900

    re-create a loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,12 @@
+import "./components.css";
+
 function Loading({ message }) {
-	return <div>{message}</div>;
+	return (
+		<div className="render-loading_spinner glassmorphism">
+			<div className="message">{message}</div>
+			<div className="spinner"></div>
+		</div>
+	);
 }
 
 export default Loading;
