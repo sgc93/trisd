c4a0ed19307906ae19e0e641fa81b245edb5c30e
sgc93 <smachewgedefaw@gmail.com>
2024-02-26 01:26:24 -0900

    remove unused files and styles

--- src/hooks/useCursor.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { CursorContext } from "../contexts/CursorContextProvider";
-
-export function useCursor() {
-	const result = useContext(CursorContext);
-	return result;
-}
