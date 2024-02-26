e288e59640c17f29ac3ff849333a8b8588aa94cf
sgc93 <smachewgedefaw@gmail.com>
2024-02-25 22:19:56 -0900

    code destructure

--- src/canvas/displayCanvas/DisplayCanvas.jsx
+++ src/canvas/displayCanvas/DisplayCanvas.jsx
@@ -1,76 +1,27 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef, useState } from "react";
+import { Suspense, useRef } from "react";
 import { useSnapshot } from "valtio";
-import ColorPicker from "../../components/ColorPicker";
 import Loading from "../../components/Loading";
-import Logo from "../../components/Logo";
-import ModelController from "../../components/ModelController";
 import proxyState from "../../proxyStore/proxy";
 import DisplayModel from "./DisplayModel";
 
-const initialController = {
-	zoom: false,
-	rotate: false,
-	movement: true,
-};
-
-function DisplayCanvas({ glbData }) {
+function DisplayCanvas({ glbData, controller }) {
 	const snap = useSnapshot(proxyState);
-	const [controller, setController] = useState(initialController);
-	const [screenshotImg, setScreenshotImg] = useState("");
-	const [displayModal, setDisplayModal] = useState(false);
-	const [isColorPicker, setIsColorPicker] = useState(false);
-
 	const { zoom, rotate } = controller;
 	const screenshot = useRef();
 
 	return (
-		snap.inCanvas && (
-			<div className="display-canvas_page" style={{ backgroundColor: snap.bg }}>
-				<Logo
-					newClass={"display-logo"}
-					handleClick={() => {
-						proxyState.inCanvas = false;
-						proxyState.inHome = true;
-					}}
-				/>
-				<ModelController
-					controller={controller}
-					setController={setController}
-					screenshot={screenshot}
-					setScreenshotImg={setScreenshotImg}
-					setDisplayModal={setDisplayModal}
-					setIsColorPicker={setIsColorPicker}
-				/>
-				{isColorPicker && <ColorPicker purpose={"themeChange"} />}
-				{displayModal && (
-					<div className="screenshot-modal_container">
-						<div className="screenshot-modal">
-							<div>
-								<h4>Your screenshot</h4>
-							</div>
-							<div className="img-container">
-								<img
-									src={screenshotImg ? screenshotImg : "/model.png"}
-									alt="model_screenshot"
-								/>
-							</div>
-						</div>
-					</div>
-				)}
-				<Suspense fallback={<Loading message={"rendering ..."} />}>
-					<div className="display-canvas">
-						<Canvas ref={screenshot}>
-							<ambientLight intensity={1.5} />
-							<spotLight position={[10, 10, 10]} castShadow />
-							<OrbitControls enableZoom={zoom} enableRotate={rotate} />
-							<DisplayModel glbData={glbData} controller={controller} />
-						</Canvas>
-					</div>
-				</Suspense>
-			</div>
-		)
+		<Suspense fallback={<Loading message={"rendering ..."} />}>
+			<Canvas ref={screenshot}>
+				<color attach={"background"} args={[snap.bg]} />
+				<fog attach={"fog"} args={[snap.bg, 5, 10]} />
+				<ambientLight intensity={1.5} />
+				<spotLight position={[10, 10, 10]} castShadow />
+				<OrbitControls enableZoom={zoom} enableRotate={rotate} />
+				<DisplayModel glbData={glbData} controller={controller} />
+			</Canvas>
+		</Suspense>
 	);
 }
 
--- src/canvas/displayCanvas/DisplayModel.jsx
+++ src/canvas/displayCanvas/DisplayModel.jsx
@@ -1,12 +1,53 @@
-import { Center, useGLTF } from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 import { useRef } from "react";
+
+import { PresentationControls, Stage } from "@react-three/drei";
 
 function DisplayModel({ glbData, controller }) {
 	const { movement } = controller;
 	const { scene } = useGLTF(glbData);
 	const model = useRef();
+
+	const texture = useTexture({
+		map: "./textures/metalic/Metal_006_basecolor.jpg",
+		normalMap: "./textures/metalic/Metal_006_normal.jpg",
+		roughnessMap: "./textures/metalic/Metal_006_roughness.jpg",
+		aoMap: "./textures/metalic/Metal_006_ambientOcclusion.jpg",
+	});
+	const terrazzo = useTexture({
+		map: "./textures/terrazzo/Terrazzo_003_basecolor.jpg",
+		normalMap: "./textures/terrazzo/Terrazzo_003_normal.jpg",
+		roughnessMap: "./textures/terrazzo/Terrazzo_003_roughness.jpg",
+		aoMap: "./textures/terrazzo/Terrazzo_003_ambientOcclusion.jpg",
+	});
+	const dirt = useTexture({
+		map: "./textures/dirt/dirt_floor_basecolor.jpg",
+		normalMap: "./textures/dirt/dirt_floor_normal.jpg",
+		roughnessMap: "./textures/dirt/dirt_floor_roughness.jpg",
+		aoMap: "./textures/dirt/dirt_floor_ao.jpg",
+	});
+	const mosaic = useTexture({
+		map: "./textures/mosaic/TilesMosaicPennyround001_COL_2K.png",
+		normalMap: "./textures/mosaic/TilesMosaicPennyround001_NRM_2K.png",
+		roughnessMap: "./textures/mosaic/TilesMosaicPennyround001_BUMP_2K.png",
+		aoMap: "./textures/mosaic/TilesMosaicPennyround001_AO_2K.png",
+		dispMap: "./textures/mosaic/TilesMosaicPennyround001_DISP_2K.png",
+	});
+	const bronze = useTexture({
+		map: "./textures/bronze/MetalBronzeWorn001_COL_2K_METALNESS.png",
+		normalMap: "./textures/bronze/MetalBronzeWorn001_NRM_2K_METALNESS.png",
+		roughnessMap:
+			"./textures/bronze/MetalBronzeWorn001_ROUGHNESS_2K_METALNESS.png",
+		dispMap: "./textures/bronze/MetalBronzeWorn001_DISP_2K_METALNESS.png",
+	});
+	const oak = useTexture({
+		map: "./textures/oak/oak_col.png",
+		normalMap: "./textures/oak/oak_nrm.png",
+		roughnessMap: "./textures/oak/oak_roughness.png",
+		dispMap: "./textures/oak/oak_disp.png",
+	});
 
 	useFrame((state, delta) => {
 		if (model.current && movement) {
@@ -22,11 +63,27 @@ function DisplayModel({ glbData, controller }) {
 	if (!glbData) return;
 
 	return (
-		<group ref={model}>
-			<Center>
-				<primitive scale={[1.5, 1.5, 1.5]} object={scene} />
-			</Center>
-		</group>
+		<PresentationControls speed={1.5} global polar={[-0.1, Math.PI / 4.5]}>
+			<Stage environment={"city"} intensity={1} castShadow={false}>
+				<primitive object={scene} />
+			</Stage>
+			<mesh rotation={[-Math.PI / 2.07, 0, Math.PI]} position={[0, 0, -35]}>
+				<planeGeometry args={[170, 170]} />
+				<meshStandardMaterial {...oak} />
+				{/* <MeshReflectorMaterial
+					blur={[1000, 200]}
+					resolution={[2049]}
+					mixBlur={1}
+					mixStrength={40}
+					roughness={1}
+					depthScale={1.2}
+					minDepthThreshold={0.4}
+					maxDepthThreshold={1.4}
+					color="#484848"
+					metalness={0.9}
+				/> */}
+			</mesh>
+		</PresentationControls>
 	);
 }
 
