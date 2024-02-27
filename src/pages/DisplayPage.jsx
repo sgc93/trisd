7fceea8a60391a57fc8fec85d0d5e38901f3fce3
sgc93 <smachewgedefaw@gmail.com>
2024-02-27 11:58:32 -0900

    set default model to be rendered when someone goes to the page with out uploading their own .glb file

--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -20,53 +20,53 @@ function DisplayPage({ glbData }) {
 	const [isColorPicker, setIsColorPicker] = useState(false);
 	const [texture, setTexture] = useState();
 
+	if (!glbData) glbData = "./sgc.glb";
+
 	function changeFloorMaterial(textureId) {
 		setTexture(textureId);
 	}
 
 	return (
-		snap.inCanvas && (
-			<div className="display-canvas_page">
-				<Logo
-					newClass={"display-logo"}
-					handleClick={() => {
-						proxyState.inCanvas = false;
-						proxyState.inHome = true;
-					}}
-				/>
-				<ModelController
-					controller={controller}
-					setController={setController}
-					setScreenshotImg={setScreenshotImg}
-					setDisplayModal={setDisplayModal}
-					setIsColorPicker={setIsColorPicker}
-				/>
-				{isColorPicker && <ColorPicker purpose={"themeChange"} />}
-				{displayModal && (
-					<div className="screenshot-modal_container">
-						<div className="screenshot-modal">
-							<div>
-								<h4>Your screenshot</h4>
-							</div>
-							<div className="img-container">
-								<img
-									src={screenshotImg ? screenshotImg : "/model.png"}
-									alt="model_screenshot"
-								/>
-							</div>
+		<div className="display-canvas_page">
+			<Logo
+				newClass={"display-logo"}
+				handleClick={() => {
+					proxyState.inCanvas = false;
+					proxyState.inHome = true;
+				}}
+			/>
+			<ModelController
+				controller={controller}
+				setController={setController}
+				setScreenshotImg={setScreenshotImg}
+				setDisplayModal={setDisplayModal}
+				setIsColorPicker={setIsColorPicker}
+			/>
+			{isColorPicker && <ColorPicker purpose={"themeChange"} />}
+			{displayModal && (
+				<div className="screenshot-modal_container">
+					<div className="screenshot-modal">
+						<div>
+							<h4>Your screenshot</h4>
+						</div>
+						<div className="img-container">
+							<img
+								src={screenshotImg ? screenshotImg : "/model.png"}
+								alt="model_screenshot"
+							/>
 						</div>
 					</div>
-				)}
-				<MaterialEditor type={"floor"} changeTMaterial={changeFloorMaterial} />
-				<div className="display-canvas">
-					<DisplayCanvas
-						controller={controller}
-						glbData={glbData}
-						texture={texture}
-					/>
 				</div>
+			)}
+			<MaterialEditor type={"floor"} changeTMaterial={changeFloorMaterial} />
+			<div className="display-canvas">
+				<DisplayCanvas
+					controller={controller}
+					glbData={glbData}
+					texture={texture}
+				/>
 			</div>
-		)
+		</div>
 	);
 }
 
