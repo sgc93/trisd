1d32f3b3952da8df9b97008fcf9dbc96455b1800
sgc93 <smachewgedefaw@gmail.com>
2024-02-27 11:59:32 -0900

    define routes with the createBrowserRouter function of react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import { useState } from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { useSnapshot } from "valtio";
 import Cursor from "./components/Cursor";
+import Loading from "./components/Loading";
 import DisplayPage from "./pages/DisplayPage";
 import HomePage from "./pages/HomePage";
 import IntroPage from "./pages/IntroPage";
 import UploadPage from "./pages/UploadPage";
 import proxyState from "./proxyStore/proxy";
+
+function CommonComponent() {
+	return (
+		<>
+			<Cursor />
+			<Outlet />
+		</>
+	);
+}
+const router = createBrowserRouter([
+	{
+		element: <CommonComponent />,
+		children: [
+			{
+				path: "/intro",
+				element: <IntroPage />,
+			},
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "*",
+				element: <Loading message={"404"} type={"notify"} />,
+			},
+			{
+				path: "/upload",
+				element: <UploadPage />,
+			},
+			{
+				path: "/display",
+				element: <DisplayPage />,
+			},
+		],
+	},
+]);
 
 function App() {
 	const snap = useSnapshot(proxyState);
@@ -14,11 +52,12 @@ function App() {
 
 	return (
 		<main className="app transition-all ease-in relative">
-			<Cursor cursorClass={cursorClass} />
+			<RouterProvider router={router} />
+			{/* <Cursor cursorClass={cursorClass} />
 			<IntroPage />
 			<HomePage />
 			<UploadPage setGlbData={setGlbData} />
-			<DisplayPage glbData={glbData} />
+			<DisplayPage glbData={glbData} /> */}
 		</main>
 	);
 }
