b7fe166d6c657528b642bb633d5834ee3f9c4dec
sgc93 <smachewgedefaw@gmail.com>
2024-02-28 01:02:48 -0900

    update contents of notification page : add animated contents

--- src/pages/NotificationPage.jsx
+++ src/pages/NotificationPage.jsx
@@ -1,5 +1,67 @@
-import "/pages.css";
+import { motion } from "framer-motion";
+import LogoSVG from "../components/LogoSVG";
+import "./pages.css";
+
+const boxVariants = {
+	initial: {
+		opacity: 0,
+		translateX: 1000,
+	},
+	animate: {
+		opacity: 1,
+		translateX: 0,
+		transition: {
+			duration: 1,
+			delay: 2,
+			ease: "easeInOut",
+		},
+	},
+};
+
+const btnVariants = {
+	initial: {
+		opacity: 0,
+		translateX: -1000,
+	},
+	animate: {
+		opacity: 1,
+		translateX: 0,
+		transition: {
+			duration: 1,
+			delay: 2,
+			ease: "easeInOut",
+		},
+	},
+};
 
 export default function NotificationPage() {
-	return <section className="notification-page">NotificationPage</section>;
+	function handleClosingTab() {
+		window.close();
+	}
+	return (
+		<section className="notification-page">
+			<LogoSVG />
+			<motion.div
+				variants={boxVariants}
+				initial="initial"
+				animate="animate"
+				className="message-box glassmorphism"
+			>
+				<span>TRISD is being updated 🔥</span>
+				<span>
+					it will come with new features and in more optimized manner ---
+					SOON!👍🏻
+				</span>
+				<motion.button
+					variants={btnVariants}
+					initial="initial"
+					animate="animate"
+					className="close-tab-btn"
+					onClick={handleClosingTab}
+				>
+					close the app
+				</motion.button>
+			</motion.div>
+		</section>
+	);
 }
