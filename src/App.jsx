eac1aba6a2a4830cb7506dea50668a1befd2f86a
sgc93 <smachewgedefaw@gmail.com>
2024-02-29 08:30:48 -0900

    add intro and home pages in the react router as one routes : but render  them conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useSnapshot } from "valtio";
 import Cursor from "./components/Cursor";
 import DisplayPage from "./pages/DisplayPage";
 import HomePage from "./pages/HomePage";
+import IntroPage from "./pages/IntroPage";
 import NotificationPage from "./pages/NotificationPage";
 import UploadPage from "./pages/UploadPage";
 import proxyState from "./proxyStore/proxy";
@@ -16,13 +17,23 @@ function CommonComponent() {
 		</>
 	);
 }
+
+function Home() {
+	return (
+		<>
+			<IntroPage />
+			<HomePage />
+		</>
+	);
+}
+
 const router = createBrowserRouter([
 	{
 		element: <CommonComponent />,
 		children: [
 			{
 				path: "/",
-				element: <HomePage />,
+				element: <Home />,
 			},
 			{
 				path: "/upload",
@@ -37,7 +48,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-	const [inUpdateState] = useState(true);
+	const [inUpdateState] = useState(false);
 	const snap = useSnapshot(proxyState);
 	const [glbData, setGlbData] = useState("./jebena.glb");
 	const [cursorClass, setCursorClass] = useState("def_cursor");
