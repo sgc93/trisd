d051aedf090ee84efe98e691e0f40076a1c27063
sgc93 <smachewgedefaw@gmail.com>
2024-03-01 10:14:08 -0900

    add displayable content of and error object

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import { useRouteError } from "react-router-dom";
+import CustomBtn from "../components/CustomBtn";
 
 export default function Error() {
 	const homeError = useRouteError();
-	console.log(homeError.message);
-	return <section className="error-page">{homeError.message + ""}</section>;
+	const [showDetail, setShowDetail] = useState();
+	console.log(homeError);
+	return (
+		<section className="error-page">
+			<div className="error-message glassmorphism">
+				<span>{homeError.message || homeError.data} </span>
+				<CustomBtn
+					type={"outlined"}
+					title={showDetail ? "hide details" : "see details"}
+					customStyles={{ fontWeight: "200" }}
+					handleClick={() => setShowDetail((showDetail) => !showDetail)}
+				/>
+			</div>
+		</section>
+	);
 }
