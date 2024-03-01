4ee13687d57092a30891c4256409936f2c0aa138
sgc93 <smachewgedefaw@gmail.com>
2024-03-01 12:13:13 -0900

    add button and detail content in the error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,20 +1,37 @@
 import { useState } from "react";
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 import CustomBtn from "../components/CustomBtn";
 
 export default function Error() {
+	const navigateTo = useNavigate();
 	const homeError = useRouteError();
 	const [showDetail, setShowDetail] = useState();
 	console.log(homeError);
 	return (
 		<section className="error-page">
 			<div className="error-message glassmorphism">
-				<span>{homeError.message || homeError.data} </span>
+				<div>
+					<span>No Internet</span>
+					<CustomBtn
+						type={"outlined"}
+						title={showDetail ? "hide details" : "Show details"}
+						customStyles={{ fontWeight: "200" }}
+						handleClick={() => setShowDetail((showDetail) => !showDetail)}
+					/>
+				</div>
+
+				<div className={`error ${showDetail ? "showError" : "hideError"}`}>
+					<span>{homeError.message || homeError.data} </span>
+					<span>
+						You Have Lost Your Connection with the Internet World, at least for
+						know😊
+					</span>
+				</div>
 				<CustomBtn
-					type={"outlined"}
-					title={showDetail ? "hide details" : "see details"}
-					customStyles={{ fontWeight: "200" }}
-					handleClick={() => setShowDetail((showDetail) => !showDetail)}
+					type={"filled"}
+					title={"back"}
+					handleClick={() => navigateTo("/display")}
+					customStyles={{ margin: "-2rem 3rem 0rem 0rem " }}
 				/>
 			</div>
 		</section>
