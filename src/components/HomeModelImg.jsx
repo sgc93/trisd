f768f284eaffa2ba1dda33751ab742cd8cc73bb1
sgc93 <smachewgedefaw@gmail.com>
2024-03-02 06:55:32 -0900

    enable changing texture with material editor pannel

--- src/components/HomeModelImg.jsx
+++ src/components/HomeModelImg.jsx
@@ -1,10 +1,10 @@
-export default function HomeModelImg() {
-	// useEffect(() => {
-
-	// })
+export default function HomeModelImg({ texture }) {
+	if (texture === "") {
+		texture = "metalic";
+	}
 	return (
 		<div>
-			<img src="./imgTextures/metalic.png" alt="golden_jebena" />
+			<img src={`./imgTextures/${texture}.png`} alt={`${texture}_jebena`} />
 		</div>
 	);
 }
