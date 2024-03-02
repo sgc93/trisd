de86fc524674c11e4f623725bded6d3faeb26d44
sgc93 <smachewgedefaw@gmail.com>
2024-03-01 23:55:11 -0900

    handle continue with 3D rendering or go with 2d versions

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,3 +1,4 @@
+import { introProxy } from "../proxyStore/proxy";
 import CustomBtn from "./CustomBtn";
 import "./components.css";
 
@@ -7,8 +8,23 @@ export default function Notification({ message, type }) {
 			<span>Notification:-</span> <span className="message">{message}</span>
 			<div className="notifier-btns">
 				<CustomBtn type={"outlined"} title={"what?"} />
-				<CustomBtn type={"outlined"} title={"continue with 2D"} />
-				<CustomBtn type={"filled"} title={"continue with 2D"} />
+				<CustomBtn
+					type={"outlined"}
+					title={"continue with 3D"}
+					handleClick={() => {
+						introProxy.isNotifying = false;
+					}}
+				/>
+				<CustomBtn
+					type={"filled"}
+					title={"continue with 2D"}
+					handleClick={() => {
+						introProxy.isNotifying = false;
+						introProxy.showBtn = true;
+						introProxy.is3DHome = false;
+						introProxy.is2DHome = true;
+					}}
+				/>
 			</div>
 		</div>
 	);
